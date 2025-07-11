7d196c6bd12807da9db80058e6a8e229229e90b5
PuneetxSingh <puneetbhatt7600@gmail.com>
2025-07-11 15:52:11 +0530

    naman bhai

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Shad cn is working</Button>
+      <Button>Shad cn isaklncwkjcjenjckne working</Button>
     </div>
   )
 }
